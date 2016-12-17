eba3f529e2d93a0f2dc7a1e6aa15804062cccf65
Mikael Eriksson <micke_eri@hotmail.com>
2016-12-17 10:41:19 +0100

    New .net core api project

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace file_reader_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FilesController : Controller
+    {
+        // GET api/files
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "file1", "file2" };
+        }
+
+        // GET api/files/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/files
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/files/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/files/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
